411875b137d8e6166e5fc033f9644c7b68660a37
Donald Oakes <donald.h.oakes@gmail.com>
2021-04-10 16:15:38 -0600

    watch css

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -1,11 +1,11 @@
-// TODO: stay on plugin-typescript 3.x pending this issue:
-// https://github.com/rollup/plugins/issues/287
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import commonjs from '@rollup/plugin-commonjs';
-import postcss from 'rollup-plugin-postcss';
 import vue from 'rollup-plugin-vue';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
 import replace from '@rollup/plugin-replace';
+import copy from 'rollup-plugin-copy-watch';
+import sass from 'node-sass';
 
 export default {
     input: 'src/main.ts',
@@ -23,6 +23,15 @@ export default {
         }),
         replace({
             'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+        copy({
+            watch: 'css/style.scss',
+            targets: [{
+                src: 'css/style.scss',
+                dest: 'out',
+                rename: 'style.css',
+                transform: (contents) => sass.renderSync({ data: contents.toString() }).css
+            }]
         })
     ]
 };
